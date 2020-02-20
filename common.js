395be12dedd109205676934d47fee591e7f6b375
yoniseo <59194226+yoniseo@users.noreply.github.com>
2020-02-20 23:41:57 +0900

    Update common.js

--- common.js
+++ common.js
@@ -2,17 +2,27 @@ $(document).ready(function(){
 	$('.bxslider').bxSlider({
 		mode: 'horizontal',
 		auto : true,
-			captions: true,
-			controls: false
+		captions: true,
+		controls: false
 	});
-});
+	
+	$('.bxslider_notice').bxSlider({
+		mode: 'horizontal',
+		auto : false,
+		captions: false,
+		controls: true,
+		pager: false,
+		infiniteLoop: false,
+		hideControlOnEnd: true,
+		speed: '10',
+	});
 
-$(document).ready(function(){
+	$('.mo_btn, .mo-gnb-shadow').on('click', function(){
+		$('html').toggleClass('show');
+	});
 	$('.mm').on('mouseenter', function(){
 		$('.mm_sub').css('display','block')
 	});
-});
-$(document).ready(function(){
 	$('.mm').on('mouseleave', function(){
 		$('.mm_sub').hide();
 	});
@@ -24,5 +34,4 @@ var nd = new Date();
 var thisMonth = mn[nd.getMonth()];
 var thisWeek = dw[nd.getDay()];
 var d = nd.getDate();
-
 $('.date').html(thisMonth+'.'+' '+d+'.'+' '+thisWeek);
